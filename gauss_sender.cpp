4db23839e4b6c6ab3306481429cc1adc5daa03c5
Mutalib Mohammed <ms17276@bristol.ac.uk>
2024-08-07 22:00:11 +0100

    Fixed bug due to incorrect index check

--- gauss_sender.cpp
+++ gauss_sender.cpp
@@ -1,11 +1,13 @@
-#include "stdexec/execution.hpp"
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <vector>
+
 #include "exec/on.hpp"
+#include "exec/repeat_n.hpp"
 #include "exec/static_thread_pool.hpp"
 #include "nvexec/stream_context.cuh"
-#include "exec/repeat_n.hpp"
-#include <vector>
-#include <algorithm>
-#include <iostream>
+#include "stdexec/execution.hpp"
 
 template <typename T>
 constexpr std::pair<T, T> wavefront_coordinates(T ny, T nx, T wavefront, uint boundary)
@@ -17,7 +19,7 @@ constexpr std::pair<T, T> wavefront_coordinates(T ny, T nx, T wavefront, uint bo
 
     T xmin = std::max(left, wavefront - ((ny - 1 - bottom)));
     T xmax = std::min(wavefront - top, nx - 1 - right);
-    return {xmin, xmax >= xmin ? xmax : xmin - 1};
+    return {xmin, xmax};
 }
 
 int main(int argc, char **argv)
@@ -29,11 +31,13 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    using type = double;
+    using type = float;
 
     const int ny = std::stoi(argv[1]);
     const int nx = std::stoi(argv[2]);
     const int n = std::stoi(argv[3]);
+
+    assert((nx % 2 == 0));
 
     std::vector<type> p_data(ny * nx), pnew_data(ny * nx);
 
@@ -70,6 +74,13 @@ int main(int argc, char **argv)
                          pnew_data[(ny - 1) * nx + i] = 10.f;
                      })));
 
+    // for (int y = 0; y < ny; y++) {
+    //     for (int x = 0; x < nx; x++) {
+    //         std::printf("%2.3f  ", p_data[y * nx + x]);
+    //     }
+    //     std::cout << '\n';
+    // }
+
     int *wavefront = new int;
     *wavefront     = 0;
 
@@ -83,25 +94,41 @@ int main(int argc, char **argv)
                                     it, wavefront](std::size_t i) {
                                        auto [xmin, xmax] =
                                            wavefront_coordinates(ny, nx, *wavefront, 0b1111);
+
                                        int ymin = *wavefront - xmin;
                                        int ymax = *wavefront - xmax;
 
-                                       if (i >= ymin * nx + xmin && i <= ymax * nx + xmax) {
+                                       int x = i & (nx - 1);
+                                       int y = i / nx;
+
+                                       //    std::printf("wavefront: %d, xmin: %d, xmax: %d, ymin:
+                                       //    %d, "
+                                       //                "ymax: %d,  %d  <= %lu <= %d , i: %lu\n",
+                                       //                *wavefront, xmin, xmax, ymin, ymax, xmin,
+                                       //                i & (nx - 1), xmax, i);
+
+                                       if (xmin <= x && x <= xmax && ymin <= y && y <= ymax) {
                                            pnew_data[i] =
                                                0.25 * (pnew_data[i - nx] + pnew_data[i - 1] +
                                                        p_data[i + nx] + p_data[i + 1]);
                                        }
                                    }) |
-
                          stdexec::then([wavefront]() { *wavefront += 1; })) |
-            exec::repeat_n(nwavefronts) | stdexec::then([wavefront]() { *wavefront = 0; });
+            exec::repeat_n(nwavefronts - 1) | stdexec::then([wavefront]() { *wavefront = 0; });
         stdexec::sync_wait(std::move(work));
         std::swap(p_data, pnew_data);
     }
 
+    for (int y = 0; y < ny; y++) {
+        for (int x = 0; x < nx; x++) {
+            std::printf("%2.3f  ", p_data[y * nx + x]);
+        }
+        std::cout << '\n';
+    }
+
     type sum = 0.f;
     for (int i = 0; i < ny * nx; i++) {
-        sum += pnew_data[i];
+        sum += p_data[i];
     }
 
     std::cout << "Sum: " << sum << std::endl;
